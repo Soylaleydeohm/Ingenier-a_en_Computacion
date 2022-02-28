f9b3294d05d7da29b49171c78ce9a55006f75606
Soylaleydeohm <48796763+Soylaleydeohm@users.noreply.github.com>
2022-02-28 14:25:19 -0300

    Delete Tester.java

--- Tester.java
+++
@@ -1,9 +0,0 @@
-
-public class Tester
-{
-    public static void main() {
-        Lista l = new Lista();       
-        GUI_SpaceObject space = new GUI_SpaceObject(l);
-        space.setVisible(true);      
-    }
-}
